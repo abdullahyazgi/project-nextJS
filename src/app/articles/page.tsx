6463d0fdc50be3a498acd95f8250fc7e1852d667
abdullahyazgi <abdullah@yazgi.se>
2024-09-27 20:18:21 +0300

    Data Fetching

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import ArticleItem from "@/components/articles/ArticleItem";
+import { Article } from "@/utils/types";
 
-const Articles = () => {
+const Articles = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const articles: Article[] = await response.json();
+
   return (
-    <div>Articles</div>
-  )
-}
+    <section className="container m-auto px-5">
+      <div className="flex items-center justify-center flex-wrap gap-7">
+        {articles.map((item) => (
+          <ArticleItem article={item} key={item.id} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default Articles
+export default Articles;
