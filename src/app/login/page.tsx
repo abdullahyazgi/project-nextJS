3179e32d43e246651e366b51fccad3d2ce4630f4
abdullahyazgi <abdullah@yazgi.se>
2024-11-06 10:03:22 +0300

    Login debug

--- src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Login = () => {
-  return (
-    <div className='fix-height'>Login</div>
-  )
-}
-
-export default Login
--- src/app/register/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Register = () => {
-  return <div className="fix-height">Register</div>;
-}
-
-export default Register
