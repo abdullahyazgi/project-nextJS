14242c976c59612808ff377ba6bf3a4f12ff4d02
abdullahyazgi <abdullah@yazgi.se>
2024-11-22 11:14:08 +0300

    building api and Route Handler

---
+++ src/app/api/articles/route.ts
@@ -0,0 +1,44 @@
+import { NextRequest, NextResponse } from "next/server";
+import { articles } from "@/utils/data";
+import { Article } from "@/utils/types";
+import { createArticleSchema } from "@/utils/validationSchemas";
+import { CreateArticleDto } from "@/utils/dtos";
+
+/**
+ *  @method GET
+ *  @route ~/api/articles
+ *  @desc Get All Articles
+ *  @access public
+ */
+export function GET(request: NextRequest) {
+    return NextResponse.json(articles, {status: 200});
+}
+
+
+/**
+ *  @method POST
+ *  @route ~/api/articles
+ *  @desc Create New Article
+ *  @access public
+ */
+export async function POST(request: NextRequest) {
+    const body = (await request.json()) as CreateArticleDto;
+
+
+
+    const validation = createArticleSchema.safeParse(body);
+    if(!validation.success) {
+        return NextResponse.json({ message: validation.error.errors[0].message }, { status: 400 });
+        
+    }
+
+    const newArticl: Article = {
+        title: body.title,
+        body: body.body,
+        id: articles.length + 1,
+        userId: 200,
+    }
+
+    articles.push(newArticl);
+    return NextResponse.json(newArticl, {status: 201});
+}
---
+++ src/utils/data.ts
@@ -0,0 +1,22 @@
+import { Article } from "./types";
+
+export const articles: Article[] = [
+  {
+    id: 101,
+    userId: 101,
+    title: "NextJs",
+    body: "Learn NextJs",
+  },
+  {
+    id: 102,
+    userId: 102,
+    title: "JavaScript",
+    body: "JavaScipt is a powerfull programing language",
+  },
+  {
+    id: 103,
+    userId: 103,
+    title: "TypeScript",
+    body: "JavaScipt is a powerfull programing language",
+  },
+];
