8131d72dbc1b951761a2d53163eafe30e66d5bb0
abdullahyazgi <abdullah@yazgi.se>
2024-11-26 17:32:59 +0300

    (Put, Delete) article

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -2,19 +2,19 @@ import { Article } from "./types";
 
 export const articles: Article[] = [
   {
-    id: 101,
+    id: 1,
     userId: 101,
     title: "NextJs",
     body: "Learn NextJs",
   },
   {
-    id: 102,
+    id: 2,
     userId: 102,
     title: "JavaScript",
     body: "JavaScipt is a powerfull programing language",
   },
   {
-    id: 103,
+    id: 3,
     userId: 103,
     title: "TypeScript",
     body: "JavaScipt is a powerfull programing language",
