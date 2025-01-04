5b0bd4544e4bca53ed4f0d2760ea5af3601f73b8
abdullahyazgi <abdullah@yazgi.se>
2025-01-04 10:16:21 +0300

    Get Articles By Search Text

--- src/apiCalls/articleApiCall.ts
+++ src/apiCalls/articleApiCall.ts
@@ -22,6 +22,18 @@ export async function getArticlesCount(): Promise<number> {
     throw new Error(`Failed to get articles count`);
   }
 
-   const { count } = await response.json() as { count:number };
-   return count;
+  const { count } = await response.json() as { count:number };
+  return count;
+}
+
+// Get articles based on searchText
+export async function getArticlesBasedOnSearch(searchText: string): Promise<Article[]> {
+  const response = await fetch(
+    `http://localhost:3000/api/articles/search?searchText=${searchText}`
+  );
+  if (!response.ok) {
+    throw new Error(`Failed to fetch articles`);
+  }
+
+  return await response.json();
 }
