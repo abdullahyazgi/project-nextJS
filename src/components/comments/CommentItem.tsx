4e6e247b3d61757a2d18a6ea4980770929ad031a
abdullahyazgi <abdullah@yazgi.se>
2025-02-15 13:00:16 +0300

    changes

--- src/components/comments/CommentItem.tsx
+++ src/components/comments/CommentItem.tsx
@@ -31,7 +31,7 @@ const CommentItem = ({ comment, userId }: CommentItemProps) => {
   return (
     <div className="mb-5 rounded-lg p-3 bg-gray-200 border-2 border-gray-300">
       <div className="flex items-center justify-between mb-2">
-        <strong className="text-gray-800 uppercase">
+        <strong className="text-gray-800">
           {comment.user.username}
         </strong>
         <span className="bg-yellow-700 px-1 rounded-lg text-white">
